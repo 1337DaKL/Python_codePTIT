6a40ea852053e43c6b8bf408e1edd70fa8eab181
1337_DaKL <luongtrinh2k3ndad@gmail.com>
2025-05-16 11:53:54 +0700

    ok

---
+++ ICPC0117.py
@@ -0,0 +1,10 @@
+set_loi_chuc = set()
+for _ in range(int(input())):
+    s = input()
+    set_loi_chuc.add(s)
+print(len(set_loi_chuc))
+# 4
+# CHUC MUNG NAM MOI
+# HAPPY NEW YEAR
+# CHUC MUNG TUOI MOI
+# CHUC MUNG NAM MOI
